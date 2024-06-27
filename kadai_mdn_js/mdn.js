627aa612e0e3481e8c4d7be4565334260faf3b01
tpmx89 <tpmx.d.728qyu@icloud.com>
2024-06-27 10:29:48 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,7 +0,0 @@
-var today = new Date(2023, 7, 22);
-
-var year = today.getFullYear();
-var month = today.getMonth() + 1;
-var day = today.getDate();
-
-console.log(year + '年', month + '月', day + '日');
